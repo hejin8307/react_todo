d81582714f418177268180e4d330d9544e5d6033
limhyejin <hejin8307@naver.com>
2023-03-16 16:30:56 +0900

    edit todolist

--- src/components/todo_list/toDoList.jsx
+++ src/components/todo_list/toDoList.jsx
@@ -5,9 +5,13 @@ import styles from './toDoList.module.css';
 const ToDoList = ({toDos, onDelete}) => {
   return (
     <section className={styles.content}>
-      {toDos.map((title) => (
-        <ToDo key={title.id} toDo={title} onDelete={onDelete} />
-      ))}
+      {toDos.length > 0 ? (
+        toDos.map((toDo) => (
+          <ToDo key={toDo.id} toDo={toDo} onDelete={onDelete} />
+        ))
+      ) : (
+        <div className={styles.message}>Please add a todo</div>
+      )}
     </section>
   );
 };
